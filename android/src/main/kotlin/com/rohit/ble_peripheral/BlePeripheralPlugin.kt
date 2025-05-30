674a9ffa60f44c4c777eca9d85bf4bb799ef20a2
Keith Bucher <keith.bucher@dapplesecurity.com>
2025-05-29 22:50:19 -0700

    PRODUCT-481: fixed issue with bonding

--- android/src/main/kotlin/com/rohit/ble_peripheral/BlePeripheralPlugin.kt
+++ android/src/main/kotlin/com/rohit/ble_peripheral/BlePeripheralPlugin.kt
@@ -287,34 +287,48 @@ class BlePeripheralPlugin : FlutterPlugin, BlePeripheralChannel, ActivityAware {
                 status: Int,
                 newState: Int,
             ) {
-                super.onConnectionStateChange(device, status, newState)
+                Log.e(TAG,"blePeripheral: onConnectionStateChange: logging: ${device.address} | $status -> $newState");
+                Log.e(TAG,"blePeripheral: onConnectionStateChange: ${device.address} | BOND_STATE: ${device.bondState}")
+                super.onConnectionStateChange(device, status, newState)                
                 when (newState) {
                     BluetoothProfile.STATE_CONNECTED -> {
+                        Log.e(TAG,"blePeripheral: onConnectionStateChange: ${device.address} | STATE_CONNECTED")
                         if (device.bondState == BluetoothDevice.BOND_NONE) {
+                            Log.e(TAG,"blePeripheral: onConnectionStateChange: ${device.address} | BOND_NONE")
                             // Wait for bonding
                             listOfDevicesWaitingForBond.add(device.address)
-                            device.createBond()
+                            // disabling to test if Android will automatically bond
+                            //device.createBond()
+                        } else if (device.bondState == BluetoothDevice.BOND_BONDING) {
+                            Log.e(TAG,"blePeripheral: onConnectionStateChange: ${device.address} | BOND_BONDING")
+                            // Device is in bonding state, wait for bond state change                            
                         } else if (device.bondState == BluetoothDevice.BOND_BONDED) {
+                            Log.e(TAG,"blePeripheral: onConnectionStateChange: ${device.address} | BOND_BONDED")
                             handler?.post {
                                 gattServer?.connect(device, true)
-                            }
-                            synchronized(bluetoothDevicesMap) {
+                            }                            
+                        }
+                        // moved from BluetoothDevice.BOND_BONDED section because onConnectionStateChange will not be called on bonding changes
+                        // per https://punchthrough.com/android-ble-guide/
+                        synchronized(bluetoothDevicesMap) {
                                 bluetoothDevicesMap.put(
                                     device.address,
                                     device
                                 )
                             }
-                        }
                         onConnectionUpdate(device, status, newState)
                     }
 
                     BluetoothProfile.STATE_DISCONNECTED -> {
+                        Log.e(TAG,"blePeripheral: onConnectionStateChange: ${device.address} | STATE_DISCONNECTED")
                         val deviceAddress = device.address
                         synchronized(bluetoothDevicesMap) { bluetoothDevicesMap.remove(deviceAddress) }
                         onConnectionUpdate(device, status, newState)
                     }
 
-                    else -> {}
+                    else -> {
+                        Log.e(TAG,"blePeripheral: onConnectionStateChange: ${device.address} | STATE_OTHER")                        
+                    }
                 }
             }
 
